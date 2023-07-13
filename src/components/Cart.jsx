eced99850e1053ed4af11ebc0bdd55a0d0f721ad
Siddharth Singh <siddharthsingh2014@gmail.com>
2023-07-13 14:37:02 +0530

    made the cart section and added it to the header component

---
+++ src/components/Cart.jsx
@@ -0,0 +1,21 @@
+export default function Cart ({ showCart, setShowCart }) {
+    if (!showCart){
+        return null;
+    }
+    return (
+        <div className="fixed top-0 left-0 w-full h-full flex justify-end z-[99]">
+            <div className="bg-black bg-opacity-50 absolute top-0 left-0 w-full h-full"></div>
+            <div className="w-full h-full bg-white relative z-[1] flex flex-col md:w-80">
+                <div className="flex items-center justify-end py-5 px-4 border-b-[1px] border-black border-opacity-10 ">
+                    <span className="grow mb-0 text-3xl font-bold font-[Pacifico] text-[#411900]">Shopping&nbsp;<span className="text-[#c32828]">Cart</span></span>
+                    <span className="flex items-center gap-1.5 cursor-pointer hover:opacity-50" onClick={() => setShowCart(false)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-4">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                        <span className="uppercase text-sm">Close</span>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,50 +1,60 @@
+import { useState } from "react";
+
+// Custom Components
+import Cart from "./Cart.jsx";
+
 export default function Header() {
+  const [showCart, setShowCart] = useState(false);
+
   return (
-    <header className="h-24 mx-auto flex items-center justify-between">
-      <div className="font-[Pacifico] pl-20">
-        <h1 className="text-[#411900] text-4xl">
-          Golden&nbsp;<span className="text-[#c32828]">Bakery</span>
-        </h1>
-      </div>
-      <div>
-        <div className="flex items-center justify-between mr-20 bg-[#c32828] rounded-xl h-12 w-28 gap-2 px-2 py-1 text-white">
-          <div className="cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
+    <>
+      <header className="h-24 mx-auto flex items-center justify-between">
+        <div className="font-[Pacifico] pl-20">
+          <h1 className="text-[#411900] text-4xl">
+            Golden&nbsp;<span className="text-[#c32828]">Bakery</span>
+          </h1>
+        </div>
+        <div className="mr-20">
+          <div className="flex items-center justify-between bg-[#c32828] rounded-xl h-12 w-28 gap-2 px-2 py-1 text-white">
+            <div className="cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </div>
+            <div className="cursor-pointer" onClick={() => setShowCart(true)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                />
+              </svg>
+            </div>
           </div>
-          <div className="cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 h-8"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-              />
-            </svg>
+          <div className="flex items-center justify-center bg-black text-white absolute h-5 w-5 rounded-full -mt-5 ml-[5.5rem]" onClick={() => setShowCart(true)}>
+            <p className="cursor-pointer">4</p>
           </div>
         </div>
-        <div className="flex items-center justify-center bg-black text-white absolute h-5 w-5 rounded-full -mt-5 ml-[5.5rem]">
-          <p className="cursor-pointer">4</p>
-        </div>
-      </div>
-    </header>
+      </header>
+      <Cart showCart={showCart} setShowCart={setShowCart} />
+    </>
   );
 }
