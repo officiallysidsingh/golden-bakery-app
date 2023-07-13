9fd2bb57da2f1694c911b76dc12e395090ec5fae
Siddharth Singh <siddharthsingh2014@gmail.com>
2023-07-13 13:14:52 +0530

    added id to all the products and added key as id for all cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,16 +13,16 @@ export default function Home() {
         ~~~~ <span className="text-[#c32828]">New</span>&nbsp;Products ~~~~
       </h1>
       <div className="grid grid-col-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-20 mx-auto max-w-[85%] mt-4">
-        {NewProducts.map((value, index) => (
-          <ProductCard data={value} key={index} />
+        {NewProducts.map((value) => (
+          <ProductCard data={value} key={value.id} />
         ))}
       </div>
       <h1 className="font-[Parisienne] mx-auto max-w-2xl text-center font-bold text-6xl mt-8">
         ~~~~ <span className="text-[#c32828]">All</span>&nbsp;Products ~~~~
       </h1>
       <div className="grid grid-col-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-20 mx-auto max-w-[85%] mt-4">
-        {AllProducts.map((value, index) => (
-          <ProductCard data={value} key={index} />
+        {AllProducts.map((value) => (
+          <ProductCard data={value} key={value.id} />
         ))}
       </div>
     </div>
