8b29f3819a64b881c1e8429ded5b5b15b2227ffa
Siddharth Singh <siddharthsingh2014@gmail.com>
2023-07-13 12:55:28 +0530

    added new products and made an all products section

---
+++ src/constants/Products.js
@@ -0,0 +1,129 @@
+import BlackForestCakeImg from "../assets/black-forest-cake.jpg";
+import Crossaint from "../assets/croissant.jpg";
+import BlueberryMuffin from "../assets/blueberry-muffin.jpg";
+import GlazedDonut from "../assets/glazed-donut.jpg";
+import TiramisuCake from "../assets/tiramisu-cake.jpg";
+import Bagel from "../assets/bagel.jpg";
+import Scone from "../assets/scone.jpg";
+import CinnamonRoll from "../assets/cinnamon-roll.jpg";
+import Cheesecake from "../assets/cheesecake.jpg";
+import FruitTart from "../assets/fruit-tart.jpg";
+import Palmier from "../assets/palmier.jpg";
+
+export const NewProducts = [
+  {
+    img: BlackForestCakeImg,
+    price: "499",
+    name: "Black Forest Cake",
+    description:
+      "Step into a world of pure indulgence with the legendary Black Forest Cake. This opulent creation is a symphony of flavors, layers, and textures that will ignite your senses. Immerse yourself in the richness of moist chocolate cake, infused with the essence of cocoa, which forms the foundation of this decadent masterpiece. Each layer is generously filled with luscious whipped cream and adorned with tart cherries, their vibrant red hue adding a touch of elegance. Topped with a cascade of chocolate shavings, this cake is a true celebration of chocolate heaven. The Black Forest Cake is an invitation to savor the ultimate pleasure and experience the luxurious bliss that only this legendary creation can provide.",
+  },
+  {
+    img: Crossaint,
+    price: "299",
+    name: "Crossaint",
+    description:
+      "Indulge in the heavenly delight of a Croissant filled with luscious chocolate and velvety cream. This delectable pastry is a symphony of flavors and textures that will transport your taste buds to new heights of satisfaction. As you take your first bite, the buttery, flaky layers of the Croissant delicately crumble, revealing a generous filling of rich, melted chocolate that oozes with every mouthful. The combination of the smooth, decadent chocolate and the light, airy pastry creates a harmonious blend of sweet and savory.",
+  },
+  {
+    img: BlueberryMuffin,
+    price: "279",
+    name: "Blueberry Muffin",
+    description:
+      "Embark on a delightful journey of taste with the Blueberry Muffin. This charming pastry captures the essence of a sunny summer morning, bursting with vibrant flavors. Each muffin boasts a moist and tender crumb, lovingly studded with juicy, plump blueberries. With every bite, the natural sweetness of the berries comes alive, creating a symphony of freshness and satisfaction. Whether enjoyed with a cup of coffee or as a quick on-the-go treat, the Blueberry Muffin is a delightful companion that will brighten your day and leave you craving for its irresistible allure.",
+  },
+  {
+    img: GlazedDonut,
+    price: "339",
+    name: "Glazed Donut",
+    description:
+      "Prepare to surrender to the sheer temptation of the Glazed Donut. Its golden and flawlessly glazed exterior beckons you with a subtle shimmer, whispering promises of sugary delight. As you take your first bite, you'll be greeted by a light, fluffy, and pillowy-soft interior that melts in your mouth. The delicate sweetness of the glaze creates a delightful contrast, satisfying your cravings with each heavenly morsel. Whether enjoyed as a morning treat or an indulgent snack, the Glazed Donut is a timeless classic that brings joy and a touch of magic to every bite.",
+  },
+  {
+    img: TiramisuCake,
+    price: "349",
+    name: "Tiramisu Cake",
+    description:
+      "Indulge in the sheer elegance of Tiramisu Cake, a true masterpiece that will transport you to the enchanting streets of Italy. With each heavenly bite, you'll be captivated by the harmonious combination of flavors and textures. Layers of delicate espresso-soaked sponge cake, infused with the richness of aromatic coffee, are lovingly embraced by a velvety mascarpone cream. Every decadent slice reveals the perfect balance of sweet and bitter, while a delicate dusting of cocoa adds a touch of indulgence. The Tiramisu Cake is a symphony of elegance and sophistication that will tantalize your taste buds and leave you longing for more.",
+  },
+];
+
+export const AllProducts = [
+  {
+    img: BlueberryMuffin,
+    price: "279",
+    name: "Blueberry Muffin",
+    description:
+    "Embark on a delightful journey of taste with the Blueberry Muffin. This charming pastry captures the essence of a sunny summer morning, bursting with vibrant flavors. Each muffin boasts a moist and tender crumb, lovingly studded with juicy, plump blueberries. With every bite, the natural sweetness of the berries comes alive, creating a symphony of freshness and satisfaction. Whether enjoyed with a cup of coffee or as a quick on-the-go treat, the Blueberry Muffin is a delightful companion that will brighten your day and leave you craving for its irresistible allure.",
+  },
+  {
+    img: FruitTart,
+    price: "659",
+    name: "Fruit Tart",
+    description:
+    "Delight in the vibrant beauty and refreshing flavors of a Fruit Tart. This exquisite pastry showcases a delicate, buttery crust, filled with a luscious pastry cream and adorned with a colorful assortment of fresh fruits. Each bite offers a delightful contrast of textures and tastes, from the crispness of the crust to the creaminess of the filling, complemented by the natural sweetness and juiciness of the fruits. Whether it's adorned with berries, kiwi, or a medley of tropical fruits, the Fruit Tart is a visual and culinary masterpiece that brings joy and a taste of elegance to any occasion.",
+  },
+  {
+    img: GlazedDonut,
+    price: "339",
+    name: "Glazed Donut",
+    description:
+    "Prepare to surrender to the sheer temptation of the Glazed Donut. Its golden and flawlessly glazed exterior beckons you with a subtle shimmer, whispering promises of sugary delight. As you take your first bite, you'll be greeted by a light, fluffy, and pillowy-soft interior that melts in your mouth. The delicate sweetness of the glaze creates a delightful contrast, satisfying your cravings with each heavenly morsel. Whether enjoyed as a morning treat or an indulgent snack, the Glazed Donut is a timeless classic that brings joy and a touch of magic to every bite.",
+  },
+  {
+    img: TiramisuCake,
+    price: "349",
+    name: "Tiramisu Cake",
+    description:
+    "Indulge in the sheer elegance of Tiramisu Cake, a true masterpiece that will transport you to the enchanting streets of Italy. With each heavenly bite, you'll be captivated by the harmonious combination of flavors and textures. Layers of delicate espresso-soaked sponge cake, infused with the richness of aromatic coffee, are lovingly embraced by a velvety mascarpone cream. Every decadent slice reveals the perfect balance of sweet and bitter, while a delicate dusting of cocoa adds a touch of indulgence. The Tiramisu Cake is a symphony of elegance and sophistication that will tantalize your taste buds and leave you longing for more.",
+  },
+  {
+    img: Scone,
+    price: "419",
+    name: "Scone",
+    description:
+    "Transport yourself to a cozy English tea room with a freshly-baked Scone. This delightful pastry offers a crumbly and tender texture, with a subtle hint of buttery goodness. Whether enjoyed plain, with clotted cream and jam, or with a variety of add-ins like dried fruit or chocolate chips, the Scone is a classic treat that pairs perfectly with a cup of tea or coffee. Each bite is a comforting embrace that brings warmth and a touch of elegance to any morning or afternoon ritual.",
+  },
+  {
+    img: Bagel,
+    price: "149",
+    name: "Bagel",
+    description:
+    "Delight in the satisfying chewiness of a Bagel, a beloved staple loved by many. This circular bread roll boasts a slightly crispy exterior and a soft, dense interior, making it the perfect canvas for endless flavor combinations. Whether you prefer a classic plain bagel, an everything bagel bursting with seeds and spices, or a savory bagel with cream cheese and lox, each bite is a journey of delightful textures and tastes. Enjoy it toasted or untoasted, plain or dressed up with your favorite toppings. The versatility of the Bagel makes it a beloved choice for breakfast, brunch, or a satisfying snack at any time of the day.",
+  },
+  {
+    img: CinnamonRoll,
+    price: "139",
+    name: "Cinnamon Roll",
+    description:
+    "Indulge in the comforting embrace of a freshly-baked Cinnamon Roll. This beloved pastry features soft, pillowy dough generously swirled with a fragrant blend of cinnamon and brown sugar. As it bakes, the heavenly aroma fills the air, making each bite even more irresistible. Topped with a luscious cream cheese glaze that melts into the warm pastry, the Cinnamon Roll is a delightful balance of sweetness and spice. Whether enjoyed as a special breakfast treat or a cozy dessert, this classic pastry is a true symbol of comfort and indulgence.",
+  },
+  {
+    img: Cheesecake,
+    price: "169",
+    name: "Cheesecake",
+    description:
+    "Delight in the velvety smoothness of a classic Cheesecake. This beloved dessert is a true testament to indulgence and elegance. With its rich and creamy texture, each slice offers a heavenly mouthfeel that is complemented by the buttery graham cracker crust. Whether topped with a luscious fruit compote, a drizzle of caramel, or enjoyed plain, the Cheesecake is a timeless favorite that never fails to captivate. From its delicate balance of sweet and tangy flavors to its decadent allure, this dessert is a true masterpiece that will leave you wanting more.",
+  },
+  {
+    img: BlackForestCakeImg,
+    price: "499",
+    name: "Black Forest Cake",
+    description:
+    "Step into a world of pure indulgence with the legendary Black Forest Cake. This opulent creation is a symphony of flavors, layers, and textures that will ignite your senses. Immerse yourself in the richness of moist chocolate cake, infused with the essence of cocoa, which forms the foundation of this decadent masterpiece. Each layer is generously filled with luscious whipped cream and adorned with tart cherries, their vibrant red hue adding a touch of elegance. Topped with a cascade of chocolate shavings, this cake is a true celebration of chocolate heaven. The Black Forest Cake is an invitation to savor the ultimate pleasure and experience the luxurious bliss that only this legendary creation can provide.",
+  },
+  {
+    img: Palmier,
+    price: "279",
+    name: "Palmier",
+    description:
+    "Indulge in the delicate crispiness of a Palmier, also known as the Elephant Ear cookie. These delightful pastries are created by folding layers of buttery puff pastry, resulting in a caramelized and flaky texture that is both sweet and savory. With their distinctive shape resembling a palm leaf, each bite offers a satisfying crunch and a hint of caramelized sugar. Whether enjoyed as a simple treat with a cup of tea or used as an elegant garnish for ice cream or other desserts, Palmiers are a versatile and delightful indulgence that brings a touch of sophistication to any occasion.",
+  },
+  {
+    img: Crossaint,
+    price: "299",
+    name: "Crossaint",
+    description:
+    "Indulge in the heavenly delight of a Croissant filled with luscious chocolate and velvety cream. This delectable pastry is a symphony of flavors and textures that will transport your taste buds to new heights of satisfaction. As you take your first bite, the buttery, flaky layers of the Croissant delicately crumble, revealing a generous filling of rich, melted chocolate that oozes with every mouthful. The combination of the smooth, decadent chocolate and the light, airy pastry creates a harmonious blend of sweet and savory.",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Banner from "../components/Banner.jsx";
 import ProductCard from "../components/ProductCard.jsx";
 
 // Constants
-import { NewProducts } from "../constants/NewProduct.js";
+import { NewProducts, AllProducts } from "../constants/Products.js";
 
 export default function Home() {
   return (
@@ -14,6 +14,14 @@ export default function Home() {
       </h1>
       <div className="grid grid-col-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-20 mx-auto max-w-[85%] mt-4">
         {NewProducts.map((value, index) => (
+          <ProductCard data={value} key={index} />
+        ))}
+      </div>
+      <h1 className="font-[Parisienne] mx-auto max-w-2xl text-center font-bold text-6xl mt-8">
+        ~~~~ <span className="text-[#c32828]">All</span>&nbsp;Products ~~~~
+      </h1>
+      <div className="grid grid-col-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-20 mx-auto max-w-[85%] mt-4">
+        {AllProducts.map((value, index) => (
           <ProductCard data={value} key={index} />
         ))}
       </div>
