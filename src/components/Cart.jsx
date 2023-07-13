7d5218d104175d0a693e1d1c8c229b9e4a8a99db
Siddharth Singh <siddharthsingh2014@gmail.com>
2023-07-13 21:49:19 +0530

    made the cart component more stylish

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,7 +4,7 @@ export default function Cart ({ showCart, setShowCart }) {
     }
     return (
         <div className="fixed top-0 left-0 w-full h-full flex justify-end z-[99]">
-            <div className="bg-black bg-opacity-50 absolute top-0 left-0 w-full h-full"></div>
+            <div className="bg-black bg-opacity-50 absolute top-0 left-0 w-full h-full" onClick={() => setShowCart(false)}></div>
             <div className="w-full h-full bg-white relative z-[1] flex flex-col md:w-80">
                 <div className="flex items-center justify-end py-5 px-4 border-b-[1px] border-black border-opacity-10 ">
                     <span className="grow mb-0 text-3xl font-bold font-[Pacifico] text-[#411900]">Shopping&nbsp;<span className="text-[#c32828]">Cart</span></span>
