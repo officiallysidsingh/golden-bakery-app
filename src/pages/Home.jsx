93b996e6df15c30e0d1ecc3acb7ce51958386b5d
Siddharth Singh <siddharthsingh2014@gmail.com>
2023-07-14 05:56:38 +0530

    made a few styling changes to render each card in between of grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
       <h1 className="font-[Parisienne] mx-auto max-w-2xl text-center font-bold text-6xl mt-8 text-[#411900]">
         ~~~~ <span className="text-[#c32828]">New</span>&nbsp;Products ~~~~
       </h1>
-      <div className="grid grid-col-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-20 mx-auto max-w-[85%] mt-4">
+      <div className="grid justify-center grid-col-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-20 mx-auto max-w-[85%] mt-4">
         {NewProducts.map((value) => (
           <ProductCard data={value} key={value.id} />
         ))}
@@ -20,7 +20,7 @@ export default function Home() {
       <h1 className="font-[Parisienne] mx-auto max-w-2xl text-center font-bold text-6xl mt-8 text-[#411900]">
         ~~~~ <span className="text-[#c32828]">All</span>&nbsp;Products ~~~~
       </h1>
-      <div className="grid grid-col-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-20 mx-auto max-w-[85%] mt-4">
+      <div className="grid justify-center grid-col-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-20 mx-auto max-w-[85%] mt-4">
         {AllProducts.map((value) => (
           <ProductCard data={value} key={value.id} />
         ))}
