a04147153658809bc4465cbdbb2ddeb40f58ca04
Siddharth Singh <siddharthsingh2014@gmail.com>
2023-07-16 10:59:19 +0530

    added the search icon and removed hamburger icon from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,19 +36,8 @@ export default function Header() {
         <div className="mr-20">
           <div className="flex items-center justify-between bg-[#c32828] rounded-xl h-12 w-28 gap-2 px-2 py-1 text-white">
             <div className="cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-8 h-8"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
               </svg>
             </div>
             <div className="cursor-pointer" onClick={() => setShowCart(true)}>
