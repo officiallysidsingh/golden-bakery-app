116412c704d6cca2e2d5ed41bd8d3b4d28b53bf4
officiallysidsingh <siddharthsingh2014@gmail.com>
2023-09-18 18:35:17 +0530

    added typedefs for product

--- server/src/graphql/product/typedefs.js
+++ server/src/graphql/product/typedefs.js
@@ -1,7 +1,36 @@
 import { gql } from "graphql-tag";
 
 export const typeDefs = gql`
+  type Product {
+    id: String!
+    name: String!
+    description: String!
+    price: String!
+    photoUrl: String!
+  }
+
+  input ProductInput {
+    name: String!
+    description: String!
+    price: String!
+    photoUrl: String!
+  }
+
+  input EditProductInput {
+    name: String
+    description: String
+    price: String
+    photoUrl: String
+  }
+
   type Query {
-    hello: String
+    getProductById(ID: ID!): Product!
+    getProductByNumber(number: Int): [Product]
+  }
+
+  type Mutation {
+    createProduct(productInput: ProductInput!): Product!
+    deleteProduct(ID: ID!): Boolean
+    editProduct(ID: ID!, editProductInput: EditProductInput!): Product!
   }
 `;
