8c591f0abbbdc88a53d25f9ce2ca2f902d2891d3
officiallysidsingh <siddharthsingh2014@gmail.com>
2023-09-21 19:21:21 +0530

    Added Query To Get Single Product Using Its Id

--- client/src/graphql.js
+++ client/src/graphql.js
@@ -1,14 +1,27 @@
 import { gql } from "@apollo/client";
 
+// Fetching Individual Product Based On Its ID
+export const GET_PRODUCT_BY_ID = gql`
+  query SingleProduct($id: ID!) {
+    getProductById(ID: $id) {
+      id
+      photoUrl
+      name
+      price
+      description
+    }
+  }
+`;
+
 // Fetching Products By Number Of Products
 export const GET_PRODUCTS_BY_NUMBER = gql`
-    query AllProducts {
-        getProductByNumber {
-            id
-            photoUrl
-            name
-            price
-            description
-        }
+  query AllProducts {
+    getProductByNumber {
+      id
+      photoUrl
+      name
+      price
+      description
     }
+  }
 `;
