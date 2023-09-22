5f617b884097f0b5c2440ec55550a29b1db514e1
officiallysidsingh <siddharthsingh2014@gmail.com>
2023-09-22 18:53:40 +0530

    Added Stripe Secret Key To .env File And Changed Code Accordingly
      TODO:
        - Add Seperate POST Route For Handling Payments
        - Add paymentService File With A Function To Handle Stripe Payments

--- server/src/utils/env-variables.js
+++ server/src/utils/env-variables.js
@@ -4,3 +4,4 @@ dotenv.config();
 // Importing and then Exporting Environment Variables
 export const PORT = process.env.PORT || 8000;
 export const MONGODB_CONNECTION_URI = process.env.MONGODB_CONNECTION_URI;
+export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
