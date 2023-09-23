7d7bcc3044fed04c5b1913b44a40214ce9965dcf
officiallysidsingh <siddharthsingh2014@gmail.com>
2023-09-23 23:13:41 +0530

    Stripe Payment Portal Done
      DONE:
        - Mapped Product Data From Client To Stripe Payment Portal
        - Sent sessionUrl For Redirecting To Stripe Portal On Checkout
        - Completed Checkpoint 3/3 of Commit 621fab38867c02fa6159a3e31f30eff3fedefb25
      TODO:
        - Create Success Page On Client
        - Create Success And Cancel URL

--- server/src/services/paymentService.js
+++ server/src/services/paymentService.js
@@ -6,36 +6,23 @@ import { STRIPE_SECRET_KEY } from "../utils/env-variables.js";
 
 // Function To Handle Stripe Payments
 export async function stripePayment(req, res) {
+  const { products } = req.body;
+
   // Load The Stripe Package With The Stripe Secret Key
   const stripe = Stripe(STRIPE_SECRET_KEY);
 
   // Mapping Line Items Object To Pass Into Stripe Session
-  const line_items = [
-    {
-      price_data: {
-        currency: "inr",
-        product_data: {
-          name: "T-Shirt",
-          description: "A Wearable",
-          images: ["https://i.ibb.co/3dsty3F/black-forest-cake.jpg"],
-        },
-        unit_amount: 213 * 100,
+  const line_items = products.map((product) => ({
+    price_data: {
+      currency: "inr",
+      product_data: {
+        name: product.name,
+        images: [product.photoUrl],
       },
-      quantity: 3,
+      unit_amount: product.price * 100,
     },
-    {
-      price_data: {
-        currency: "inr",
-        product_data: {
-          name: "Jeans",
-          description: "A Wearable",
-          images: ["https://i.ibb.co/3dsty3F/black-forest-cake.jpg"],
-        },
-        unit_amount: 2500 * 100,
-      },
-      quantity: 3,
-    },
-  ];
+    quantity: product.quantity,
+  }));
 
   // Create A Checkout Session
   const session = await stripe.checkout.sessions.create({
@@ -47,5 +34,5 @@ export async function stripePayment(req, res) {
   });
 
   // Send Redirection Link To Checkout Page
-  res.redirect(303, session.url);
+  res.json({ sessionUrl: session.url });
 }
