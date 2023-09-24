283588437e4702b6720b824f0543b039a582fb5e
officiallysidsingh <siddharthsingh2014@gmail.com>
2023-09-24 19:49:28 +0530

    Added New Route For Success Page Of Stripe Payment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 // Custom Pages
 import Layout from "./Layout";
 import Home from "./pages/Home.jsx";
+import SuccessPage from "./pages/SuccessPage.jsx";
 import ProductPage from "./pages/ProductPage";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="/success" element={<SuccessPage />} />
         <Route path="/product/:id" element={<ProductPage />} />
       </Route>
     </Routes>
