0b3aba33467633b387f494e6aeaa6cdb64a35083
officiallysidsingh <siddharthsingh2014@gmail.com>
2023-09-28 00:10:35 +0530

    Made Loader With A Spinner

---
+++ client/src/pages/LoadingPage.jsx
@@ -0,0 +1,9 @@
+import Loader from "../components/Loader.jsx";
+
+export default function Loading() {
+  return (
+    <div className="w-full h-[80vh] flex justify-center items-center">
+      <Loader />
+    </div>
+  );
+}
